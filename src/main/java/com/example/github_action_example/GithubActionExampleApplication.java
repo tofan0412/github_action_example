758f8c8ec142e5e8454973e387b152035918d282
조웅현(LGUPLUS) <woonghyuncho@lguplus.co.kr>
2022-07-18 14:24:41 +0900

    first commit

---
+++ src/main/java/com/example/github_action_example/GithubActionExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.github_action_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionExampleApplication.class, args);
+    }
+
+}
